cf42a50d756a134d7b19c15dd668b1673a7c9b20
idweixun-hy <idweixun@qq.com>
2019-01-17 13:09:02 +0800

    Site updated: 2019-01-17 13:08:48

---
+++ js/src/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e6b35016e103187657a1b801f8eaabce";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
